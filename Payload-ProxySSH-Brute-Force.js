c50baf3e38bcdc9fde46b082e4520d2483408da5
Matheus Maia <105084847+Anonimo055x@users.noreply.github.com>
2023-01-09 03:47:39 -0300

    Add files via upload

---
+++ Payload-ProxySSH-Brute-Force.js
@@ -0,0 +1,44 @@
+<script>
+    open ("GET: ") = www.replit.com/
+open = ("ProxySSH.js") = executar
+    <script>
+<iframe> src = ("ProxySSH.js") href = "OK"/></iframe>
+<open> = html = ("ProxySSH.js") <open>
+<open> = (script = ("ProxySSH.js") <open>
+<html>
+<body>
+<script>
+    window.setTimeout('location.reload(true), 30);
+</script>
+<div import time = (30000000000000000) <div>
+<script>
+    open = ("ProxySSH.js") = executar
+</script>
+<import src = ("ProxySSH.js") <import>
+<import src = ("executar") <import>
+<div import time = (30000000000000000) <div>
+<script>
+    open = ("ProxySSH.js") = executar
+</script>
+<import src = ("ProxySSH.js") <import>
+<import src = ("executar") <import>
+<div import time = (30000000000000000) <div>
+<script>
+    open = ("ProxySSH.js")) = executar
+</script>
+<import src = ("ProxySSH.js") <import>
+<import src = ("executar") <import>
+<div import time = (30000000000000000) <div>
+<script>
+    open = ("ProxySSH.js") = executar
+</script>
+<import src = ("ProxySSH.js") <import>
+<import src = ("executar") <import>
+<div import time = (30000000000000000) <div>
+<script>
+    open = ("ProxySSH.js")) = executar
+<script>
+<script>
+<import src = ("executar") <import>
+<body>
+<html>
---
+++ ProxySSH.js
@@ -0,0 +1,105 @@
+namespace VisualRun {
+    void *internal
+    {
+        {
+            void *Program
+
+        };
+        static void Main(string[] args);
+        internal
+        { ;
+            class Executor {
+                private:
+                var executar;
+
+                public:
+                    Executor() {
+                var open = (const var &) ("class");
+                var executar = executar;
+            {
+
+                class OpenBackupEventLog;
+
+                public OpenBackupEventLog(OpenBackupEventLog const &openBackupEventLog)
+            {
+                object value = (const object &) ("class");
+            }
+
+            class IMPORT_OBJECT_CODE {
+                private:
+                    IMPORT_OBJECT_CODE https;
+
+                public:
+                    IMPORT_OBJECT_CODE() {
+                var url = (const var &) ("https://replit.com/");
+            }
+        };
+
+            internal class DefaultProxySection {
+            public
+
+            DefaultProxySection(object https) {
+            var url = ("https://replit.com/");
+        }
+        }
+
+            class DebuggerTypeProxyAttribute {
+                public
+
+                DebuggerTypeProxyAttribute() {
+                    var url = ("https://replit.com/");
+                }
+
+                public
+                object https{get;
+                private set;}
+        }
+
+            int get;
+
+            class IWebProxy {
+                public:
+                    IWebProxy() {
+                var url = (const var &) ("https://replit.com/");
+            }
+
+            public:
+                object https{get;}
+        }
+
+            class AssemblyNameProxy {
+                public:
+                    AssemblyNameProxy() {
+                var url = (const var &) ("https://replit.com/");
+            }
+        }
+
+            class ProxyAttribute {
+            }
+            var url = (const var &) ("https://replit.com/");
+        }
+            class ParameterizedThreadStart
+            {
+                public ParameterizedThreadStart()
+                {
+                    var parameterizedthreadstart = ("Payload-ProxySSH-Brute-Force.js");
+                }
+                import parameterizedthreadstart = ("Payload-ProxySSH-Brute-Force.js");
+            }
+
+            class DllImportSearchPath
+            {
+                public DllImportSearchPath()
+                {
+                    var parameterizedthreadstart = ("Payload-ProxySSH-Brute-Force.js");
+                }
+                import parameterizedthreadstart = ("Payload-ProxySSH-Brute-Force.js");
+            }
+            class DllImportSearchPath
+            {
+                public DllImportSearchPath()
+                {
+                    var parameterizedthreadstart = ("Payload-ProxySSH-Brute-Force.js");
+                }
+                import parameterizedthreadstart = ("Payload-ProxySSH-Brute-Force.js");
+            }
